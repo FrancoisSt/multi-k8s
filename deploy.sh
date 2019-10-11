807a2f3de285300b32f232750b3964de351a183d
Francois Stander <francois@liebengroup.co.za>
2019-10-11 15:31:44 +0200

    fix deploy.sh errors

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,15 @@ docker build -t nawu/multi-server:latest -t nawu/multi-server:$SHA -f ./server/D
 docker build -t nawu/multi-worker:latest -t nawu/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
 docker push nawu/multi-client:latest
-docker push nawu/multi-client:$SHA
 docker push nawu/multi-server:latest
-docker push nawu/multi-server:$SHA
 docker push nawu/multi-worker:latest
+
+docker push nawu/multi-client:$SHA
+docker push nawu/multi-server:$SHA
 docker push nawu/multi-worker:$SHA
 
 kubectl apply -f k8s
 
-kubectl set image deployments/client-deployment server=nawu/multi-client:$SHA
+kubectl set image deployments/client-deployment client=nawu/multi-client:$SHA
 kubectl set image deployments/server-deployment server=nawu/multi-server:$SHA
-kubectl set image deployments/worker-deployment server=nawu/multi-worker:$SHA
+kubectl set image deployments/worker-deployment worker=nawu/multi-worker:$SHA
